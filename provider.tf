cf69e148d8bd7b411eed618179a4d7a64401a821
Richard Aguirre <richardaguirre1@gmail.com>
2025-04-10 16:12:28 -0400

    Primer commit

---
+++ provider.tf
@@ -0,0 +1,25 @@
+/*  provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["c:\\Users\\UsuarioX\\.aws\\terrafrom"]
+} 
+ */
+
+/* terraform {
+  required_providers {
+    aws = {
+      source  = "local/aws"
+      version = "5.94.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+} */
+
+/* provider "installation" {
+  filesystem_mirror {
+    path    = "C:\\Users\\UsuarioX\\.ssh\\terraform"
+    include = ["hashicorp/aws"]
+  }
+} */
